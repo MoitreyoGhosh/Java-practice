fc2eb6189e3bd99783dda8d579173ac66fafb33f
MoitreyoGhosh <moitreyoghosh@gmail.com>
2025-01-30 23:15:53 +0530

    Division program

---
+++ java/learning_java/src/Division.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Division {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        if(y==0){
+            System.out.println("Invalid output");
+        }
+        else{
+            int quotient = x / y;
+            int remainder = x % y;
+            System.out.println("The Quotient is " + " = " + quotient);
+            System.out.print("The Remainder is " + " = " + remainder);
+        }
+
+        sc.close();
+    }
+}
