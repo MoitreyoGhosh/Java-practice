b772b4bb8060bbd866fc45e56ece33ad21e9e7fe
MoitreyoGhosh <moitreyoghosh@gmail.com>
2025-02-05 14:39:57 +0530

    Week 2 Program 5

---
+++ java/learning_java/src/BookW2A5.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class BookW2A5 {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String title = sc.next();
+            String author = sc.next();
+
+            Book book = new Book();
+
+            book.setTitle(title);
+            book.setAuthor(author);
+
+            book.displayDetails();
+        }
+    }
+
+    class Book {
+        private String title;
+        private String author;
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public void setAuthor(String author) {
+            this.author = author;
+        }
+
+        public String getTitle() {
+            return this.title;
+        }
+
+        public String getAuthor() {
+            return this.author;
+        }
+
+        public void displayDetails() {
+            System.out.println("Title: " + this.title);
+            System.out.print("Author: " + this.author);
+        }
+    }
